afe052a650e1c5e4fd93f178e32a56adacaeaea6
Anis Lazaar <anis00723@gmail.com>
2023-03-29 12:52:42 +0100

    :ambulance: reset score on restart, and show score on game over

--- src/state.rs
+++ src/state.rs
@@ -50,6 +50,7 @@ impl State {
     self.player = Player::new(5, 25);
     self.frame_time = 0.0;
     self.mode = GameMode::Playing;
+    self.score = 0;
   }
 
   fn menu_actions(&mut self, ctx: &mut BTerm) {
@@ -74,6 +75,7 @@ impl State {
   fn dead(&mut self, ctx: &mut BTerm) {
     ctx.cls();
     ctx.print_centered(5, "You are dead!");
+    ctx.print_centered(6, &format!("You earned {} points", self.score));
     ctx.print_centered(8, "(P) Play Again");
     ctx.print_centered(9, "(Q) Quit Game");
 
